6f043a749b851daeb4e46032fbcbb4808e4da8af
JakubCzerwiec <jacob.czerwiec@gmail.com>
2023-05-23 06:16:04 +0200

    Almost done with Rectangle class

--- area_calc.py
+++ area_calc.py
@@ -13,5 +13,25 @@ class Rectangle() :
         area = self.set_width() * self.set_height()
         return area
 
-rec = Rectangle(8, 5)
-print(rec.get_area())
+    def get_perimeter(self) :
+        perimeter = (2 * self.set_width() + 2 * self.set_height())
+        return perimeter
+    
+    def get_diagonal(self) :
+        diagonal = ((self.set_width() ** 2 + self.set_height() ** 2) ** .5)
+        return diagonal
+
+    def get_picture(self) :
+        picture = ''
+        i = 0
+        if self.set_height() > 50 or self.set_width() > 50 :
+            print('Too big for picture.')
+            quit()
+
+        while i < self.set_height() :
+            picture += '*' * self.set_width() + '\n'
+            i += 1
+        return picture
+
+rec = Rectangle(2, 3)
+print(rec.get_picture())
